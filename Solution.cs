1a571dac4590d1f4425f8a2c2746e19e252bcec8
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-06-03 22:28:43 -0400

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int AddDigits(int num) {
+        int n = num;
+    int sum = 0;
+
+    while(n != 0){
+        sum = sum + n % 10;
+        n = n / 10;
+        if(n == 0){
+            if(sum >= 10){
+                n = sum;
+                sum = 0;
+            }
+        }
+    }
+    return(sum);
+    }
+}
